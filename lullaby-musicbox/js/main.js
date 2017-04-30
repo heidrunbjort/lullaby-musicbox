3a3473f775d099d79864dfa01a7c702bdaf662bd
djammari <adpetursson@gmail.com>
2017-04-30 14:26:57 +0000

    26.04 & 27.04 & 28.04 js update

--- lullaby-musicbox/js/main.js
+++ lullaby-musicbox/js/main.js
@@ -1,13 +1,15 @@
 window.onload = function (event) {
     
     // Some variables we will be using later
-    var camera, scene, renderer;
+    let camera, scene, renderer;
     var controls;
-    var element, container;
-    var speed;
-    
+    let element, container;
+    let speed;
+    let mouse, raycaster;
+    const clickObjects = [];
+
     // A clock to keep track of time in a convenient way
-    var clock = new THREE.Clock();
+    const clock = new THREE.Clock();
     
     // Initialize the scene, cameras, objects
     init();
@@ -28,30 +30,30 @@ window.onload = function (event) {
         
         // Create camera and position it in space
         camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
-        camera.position.set(0, 90, 0)
+        camera.position.set(0, 0, 0)
         camera.lookAt(new THREE.Vector3(0, -50, -150));
         scene.add(camera);
 
-      	var light1 = new THREE.AmbientLight( 0x404040, 4 ); // soft white light
-      	light1.position.set(0, 0, -300 );
-		 scene.add( light1 );
-		// var light2 = new THREE.PointLight( 0xff0000, 1, 200 );
-		// light2.position.set( 50, 50, 50 );
-		// scene.add( light2 );
+        const light1 = new THREE.AmbientLight( 0x404040, 6 ); // soft white light
+        light1.position.set(0, 0, 0 );
+         scene.add( light1 );
+        // var light2 = new THREE.PointLight( 0xff0000, 1, 200 );
+        // light2.position.set( 50, 50, 50 );
+        // scene.add( light2 );
         
-		var spotLight = new THREE.SpotLight( 0xffffff, 0.2 );
-		spotLight.position.set( 10, 70, 70 );
+        const spotLight = new THREE.SpotLight( 0xffffff, 0.2 );
+        spotLight.position.set( 10, -120, 70 );
 
-		spotLight.castShadow = true;
+        spotLight.castShadow = true;
 
-		spotLight.shadow.mapSize.width = 1024;
-		spotLight.shadow.mapSize.height = 1024;
+        spotLight.shadow.mapSize.width = 1024;
+        spotLight.shadow.mapSize.height = 1024;
 
-		spotLight.shadow.camera.near = 500;
-		spotLight.shadow.camera.far = 4000;
-		spotLight.shadow.camera.fov = 30;
+        spotLight.shadow.camera.near = 500;
+        spotLight.shadow.camera.far = 4000;
+        spotLight.shadow.camera.fov = 30;
 
-		scene.add( spotLight );
+        scene.add( spotLight );
 
         // Allows navigating the scene via mouse
 
@@ -59,11 +61,11 @@ window.onload = function (event) {
 
   //       controls = new THREE.OrbitControls(camera, element);
   //        controls.target.set(
-		//     camera.position.x + 0.1,
-		//     camera.position.y,
-		//     camera.position.z
-		    
-		// );
+        //     camera.position.x + 0.01,
+        //     camera.position.y,
+        //     camera.position.z
+            
+        // );
 
 
 
@@ -75,40 +77,85 @@ window.onload = function (event) {
         // cube.position.set(0, 0, 0);
 
 
-        	//loada jsonloader
-			// var loader = new THREE.JSONLoader();
-			// loader.load("obj/box24.json", function(geometry, materials){
-			// 	console.log("jeee");
-			// 	var obj = new THREE.Mesh(geometry, materials);
-			// 	obj.scale.set(1,1,1);
-			// 	obj.position.set(-10, -20, 0);
-			// 	scene.add(obj);
-			// 	console.log(obj);
-			// });
+            //loada jsonloader
+            // var loader = new THREE.JSONLoader();
+            // loader.load("obj/box24.json", function(geometry, materials){
+            //  console.log("jeee");
+            //  var obj = new THREE.Mesh(geometry, materials);
+            //  obj.scale.set(1,1,1);
+            //  obj.position.set(-10, -20, 0);
+            //  scene.add(obj);
+            //  console.log(obj);
+            // });
 
-			// var loader = new THREE.ObjectLoader();
-			// loader.load("obj/box25.json", function(obj){
-			// 	obj.scale.set(1,1,1);
-			// 	obj.position.set(0,0,0);
-			// 	scene.add(obj);
-			// 	console.log(obj);
-			// })
+            // var loader = new THREE.ObjectLoader();
+            // loader.load("obj/box25.json", function(obj){
+            //  obj.scale.set(1,1,1);
+            //  obj.position.set(0,0,0);
+            //  scene.add(obj);
+            //  console.log(obj);
+            // })
 
-			//þetta virkar með fyrra boxinu
-			var loader = new THREE.ObjectLoader();
+            //þetta virkar með fyrra boxinu
+            var loader = new THREE.ObjectLoader();
 
-			loader.load("obj/mnota5.json", function(obj){
-				obj.scale.set(3.5, 3.5, 3.5);
-				obj.position.set(30, -100, -150);
-				 obj.rotation.x = Math.PI / 0.2;
-				 //obj.rotation.y = Math.PI * 1.97;
-				//obj.rotation.z = Math.PI * 2.0;
-				scene.add(obj);
+            loader.load("obj/mnota5.json", (obj) =>{
+                obj.scale.set(3.5, 3.5, 3.5);
+                obj.position.set(30, -100, -150);
+                obj.rotation.x = 180 * (Math.PI / 180);
+                obj.rotation.y = 1.2 * (Math.PI / 180) * -1;
+                 //obj.rotation.y = Math.PI * 1.97;
+                //obj.rotation.z = Math.PI * 2.0;
+                scene.add(obj);
 
-				// obj.addEventListener("dblclick", function{
-				// 	camera.position.set()
-				// })
-			});
+                scene.traverse(function(children){
+
+                    clickObjects.push(children);
+                    //console.log(clickObjects);
+                    console.log(clickObjects[31]);
+                })
+                
+            });
+
+        let isFarNear = true;
+        
+
+        raycaster = new THREE.Raycaster();
+        mouse = new THREE.Vector2();
+
+        document.addEventListener('dblclick', onDocumentMouseDown, false);
+
+        function onDocumentMouseDown(event){
+            event.preventDefault();
+
+            mouse.x = (event.clientX / renderer.domElement.width) * 2 - 1;
+            mouse.y = (event.clientY / renderer.domElement.height) * 2 + 1;
+
+            raycaster.setFromCamera(mouse, camera);
+            const intersects = raycaster.intersectObjects(clickObjects);
+
+            if(intersects.length > 0 && isFarNear == true){
+                cameraMoveInY();
+                cameraMoveInZ();
+                isFarNear = false;
+            }
+
+            else if(intersects.length > 0 && isFarNear == false){
+                cameraMoveOutY();
+                cameraMoveOutZ();
+                
+
+                // if(intersects.lenght == clickObjects[31]){
+                //  console.log("cylender clicked");
+                // }
+                isFarNear = true;
+            }
+        }
+
+// function rotateCylender(){
+//          clickObjects[31].rotation.z =0.1;
+//      }
+
 
         // document.addEventListener("mousedown", function(e){
         //     console.log("blabla");
@@ -118,13 +165,13 @@ window.onload = function (event) {
         // },false);
         
         // Add surrounding
-        var surroundingGeometry = new THREE.CubeGeometry(200,400,600);
+        const surroundingGeometry = new THREE.CubeGeometry(200,400,600);
         var loader = new THREE.TextureLoader();
-        loader.load('blatt.jpg', function(texture) {
-            var surroundingMaterial = new THREE.MeshBasicMaterial({ 
+        loader.load('blatt.jpg', (texture) =>{
+            const surroundingMaterial = new THREE.MeshBasicMaterial({ 
                 map: texture, side: THREE.DoubleSide 
             });
-            var surroundingBox = new THREE.Mesh( surroundingGeometry, surroundingMaterial );
+            const surroundingBox = new THREE.Mesh( surroundingGeometry, surroundingMaterial );
             surroundingBox.position.y = 100;
             scene.add( surroundingBox );
         });
@@ -137,84 +184,124 @@ window.onload = function (event) {
         
     }
 
+//      var raycaster = new THREE.Raycaster();
+//      var mouse = new THREE.Vector2();
 
-    // var raycaster = new THREE.Raycaster();
-    // var mouse = new THREE.Vector2();
+//      function onMouseMove( event ) {
 
-    // function onMouseMove(e){
-    // 	mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
-    // 	mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
-    // 	console.log(raycaster);
-    // 	console.log(mouse); 
-    // }
+//          // calculate mouse position in normalized device coordinates
+//          // (-1 to +1) for both components
+
+//          mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+//          mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+//      }
+
+
+// window.addEventListener( 'mousemove', onMouseMove, false );
+
+// window.requestAnimationFrame(render);
+
+    document.addEventListener('keyboard', keyboard, false);
+
+    function keyboard(){
+        switch(event.keyCode){
+            case 0: 
+
+        }
+    }
+
+
+
     // Render loop
     function render() {
-    	//raycaster.setFromCamera(mouse, camera);
+
+   //       raycaster.setFromCamera( mouse, camera );
+
+            // var intersects = raycaster.intersectObjects( scene.children );
+
+            // for ( var i = 0; i < intersects.length; i++ ) {
+
+            //  intersects[ i ].object.material.color.set( 0xff0000 );
+
+            // }
+
+
         requestAnimationFrame(render);
         renderer.render(scene, camera);
-       
-        //controls.update();
+        // controls.update();
+
     }
     
 
     // Adjust sizes on window resize
     function handleResize() {
-        var width = window.innerWidth;
-        var height = window.innerHeight;
+        const width = window.innerWidth;
+        const height = window.innerHeight;
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
         renderer.setSize(width, height);
     }
 
-window.cm = function() {
-      	cameraMoveInY();
+    window.cm = function() {
+        cameraMoveInY();
         cameraMoveInZ();
     }
-window.cn = function(){
-	cameraMoveOutY();
-	cameraMoveOutZ();
+    window.cn = function(){
+        cameraMoveOutY();
+        cameraMoveOutZ();
 }
-    
+       // x += 0.01;
+       //  var delta = 400;
+       //  console.log(Math.sin(x) * (delta/2) + ( camera.position.z - ( (delta/2) * -1 ) ) );
     function cameraMoveInY() {
         
-        var y = 0;
-        var controlReached = false;
-        camera.updateProjectionMatrix();
-        var move = setInterval(function() {
+        let y = 0;
+        let delta = 50;
+        let controlReached = false;
+        const move = setInterval(() => {
             
             // Check if we need to stop the animation
-            if ( controlReached && (camera.position.y <= 80) ) {
+            if ( controlReached && (camera.position.y <= 15) ) {
                 clearInterval(move);
             }
             
             // Increase modifier
-            y+=0.1;
+            y += 0.049;
 
             // Move camera a little bit
-            camera.position.y = (Math.sin(y) * 40) + 90;
-            console.log(camera.position);
-            if (camera.position.y > 80) {
+            camera.position.y = Math.sin(y) * delta;
+            //console.log(camera.position.y);
+            
+            if (camera.position.y > 15) {
+                console.log('Y finished');
                 controlReached = true;
             }
+
             camera.lookAt(new THREE.Vector3(0, -50, -150));
+            camera.updateProjectionMatrix();
+
         }, 100);
         
     }
     
     function cameraMoveInZ() {
         
-        var z = 0;
-        var controlReached = false;
+        let z = 0;
+        let delta = 150;
+        let controlReached = false;
         
-        var move = setInterval(function() {
+        const move = setInterval(() => {
             
             // Increase modifier
-            z +=0.05;
+            z += 0.032;
 
             // Move camera a little bit
-            camera.position.z = (Math.sin(z) * 150 * -1);
-            console.log(camera.position);
+            camera.position.z = Math.sin(z) * delta * -1;
+            console.log(camera.position.z);
+
             if (camera.position.z < -145) {
+                console.log('Z finished');
                 clearInterval(move);
             }
             
@@ -226,45 +313,51 @@ window.cn = function(){
 
      function cameraMoveOutY() {
         
-        var y = 0;
-        var controlReached = false;
-        camera.updateProjectionMatrix();
-        var move = setInterval(function() {
+        let y = 0;
+        let delta = 15;
+        let controlReached = false;
+        const move = setInterval(() =>{
             
             // Check if we need to stop the animation
-            if ( controlReached && (camera.position.y >= 50) ) {
+            if ( controlReached && (camera.position.y <= 1) ) {
                 clearInterval(move);
             }
             
             // Increase modifier
-            y+=0.1;
+            y += 0.059;
 
             // Move camera a little bit
-            camera.position.y = (Math.sin(y) * 40) + 90;
+            camera.position.y = Math.sin(y) * delta +15;
+            //vconsole.log(camera.position.y);
             
-            if (camera.position.y < 51) {
+            if (camera.position.y > 1) {
+                //console.log('Y finished');
                 controlReached = true;
             }
-            console.log(camera.position);
+
             camera.lookAt(new THREE.Vector3(0, -50, -150));
+            camera.updateProjectionMatrix();
+
         }, 100);
         
     }
     
     function cameraMoveOutZ() {
         
-        var z = 0;
-        var controlReached = false;
+        let z = 0;
+        let delta = 150;
+        let controlReached = false;
         
-        var move = setInterval(function() {
+        const move = setInterval(() => {
             
             // Increase modifier
-            z -=0.05;
+            z += 0.032;
 
             // Move camera a little bit
-            camera.position.z = (Math.sin(z) * 150);
-            console.log(camera.position);
-            if (camera.position.z <= 0) {
+            camera.position.z = Math.sin(z) * delta - 150; 
+            console.log(camera.position.z);
+            if (camera.position.z > -1) {
+                console.log('Z finished');
                 clearInterval(move);
             }
             
